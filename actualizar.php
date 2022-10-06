f5d9048e0add84ed8f3c01531f6b62b38b775313
mario <marioalmandoz6@gmail.com>
2022-10-06 13:14:12 +0200

    organizado

---
+++ actualizar.php
@@ -0,0 +1,22 @@
+<?php
+inclue("cn.php");
+
+$nombre = $_POST["nombre"];
+$apellidos = $_POST["apellidos"];
+$dni = $_POST["dni"];
+$telefono = $_POST["telefono"];
+$fechaN = $_POST["fechaN"];
+$email = $_POST["email"];
+$clave = $_POST["clave"];
+
+$actualizar = "UPDATE usuario SET nombre='$nombre', apellido='$apellidos', DNI='$dni', telefono= '$telefono', fechaN='$fechaN', email='$email', contraseña='$clave' where email='' ";
+$resultado = mysqli_fetch_array($conn, $actualizar);
+if($resultado) {
+    echo "<script>alert('Se han modificado los datos del usuario con exito');window.location='/usuarioIdentificado.php'</script>";
+
+    
+}else{
+    echo "<script>alert('No se han podido modificar los datos del usuario'); window.location='/inicio.php'</script>";
+}
+
+?>
--- index.php
+++ index.php
@@ -1,28 +1,28 @@
+
 <?php
-  echo '<h1>Yeah, it works!<h1>';
+include("cn.php");
+
+  //echo '<h1>Yeah, it works!<h1>';
   // phpinfo();
-  $hostname = "db";
-  $username = "admin";
-  $password = "test";
-  $db = "database";
+ // $hostname = "db";
+ // $username = "admin";
+ // $password = "test";
+ // $db = "database";
 
-  $conn = mysqli_connect($hostname,$username,$password,$db);
-  if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-  }
-
-$query = mysqli_query($conn, "SELECT * FROM usuarios")
-   or die (mysqli_error($conn));
-
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td>{$row['id']}</td>
-    <td>{$row['nombre']}</td>
-   </tr>";
-   
-}
+ // $conn = mysqli_connect($hostname,$username,$password,$db);
+ // if ($conn->connect_error) {
+   // die("Database connection failed: " . $conn->connect_error);
+ // }
 ?>
+<?php
+include("inicio.php")
+?>
+<?php
+//include("registro.php");
+?>
+
+
+<!--
 <!DOCTYPE html>
     <html>
     <head>
@@ -35,49 +35,21 @@ while ($row = mysqli_fetch_array($query)) {
     </head>
 
     <body>
-        
-        
-            <form action="" method="POST" id="form">
-                <div class="form">
-                    <h1>Registro</h1>
-                <div class="grupo">
-                    <input type="text" name="" id="nombre" ><span class="barra"></span>
-                    <label for="">Nombre</label>
-                </div>
-                <div class="grupo">
-                    <input type="text" name="" id="apellidos"  ><span class="barra"></span>
-                    <label for="">Apellidos</label>
-                </div>
-                <div class="grupo">
-                    <input type="text" name="" id="dni"  ><span class="barra"></span>
-                    <label for="">DNI</label>
-                </div>
-                <div class="grupo">
-                    <input type="text" name="" id="telefono"  ><span class="barra"></span>
-                    <label for="">Telefono</label>
-                </div>
-                <div class="grupo">
-                    <input type="date" name="" id="fecha_nac" ><span class="barra"></span>
-                    <label for="">Fecha Nacimiento</label>
-                </div>
-                <div class="grupo">
-                    <input type="email" name="" id="email" ><span class="barra"></span>
-                    <label for="">E-mail</label>
-                </div>
-                <div class="grupo">
-                    <input type="password" name="" id="password1"  ><span class="barra"></span>
-                    <label for="">Contraseña</label>
-                </div>
-                <div class="grupo">
-                    <input type="password" name="" id="password12" ><span class="barra"></span>
-                    <label for="">Repita la contraseña</label>
-                </div>
+        <div class="conatiner-table">
+           
+             <?php /*
+             $resultado = mysqli_query($conn,"SELECT * FROM usuario");
                 
-                <button type="submit">Regístrate</button>
-                <p class="warnings" id="warnings"></p>
-                </div>
-            </form>
+
+             while($row = mysqli_fetch_array($resultado)) { ?>
+                <div class="table__item"><?php echo $row["nombre"];?></div>
+                <div class="table__item"><?php echo $row["apellido"];?></div>
+                <div class="table__item"><?php echo $row["DNI"];?></div>
+                <div class="table__item"><?php echo $row["telefono"];?></div>
+                <div class="table__item"><?php echo $row["fechaN"];?></div>
+                <div class="table__item"><?php echo $row["email"];?></div>
+                <?php } */?>
+        </div>        
     </body>
 </html>
-
-
+             -->
