c2369ca2dd2b5d65693c0957666a15859771fec4
mario <marioalmandoz6@gmail.com>
2022-10-08 15:39:23 +0200

    procesar

---
+++ app/procesar_editar.php
@@ -0,0 +1,20 @@
+<?php
+include("cn.php");
+
+$id = $_POST["id"];
+$nombre = $_POST["nombre"];
+$metroCuadrado = $_POST["metroCuadrado"];
+$localizacion = $_POST["localizacion"];
+$responsable = $_POST["responsable"];
+
+//Actalizar los datos
+$actualizar = "UPDATE Trastero set nombre='$nombre', metroCuadrado='$metroCuadrado', localizacion='$localizacion', responsable='$responsable' WHERE id='$id'";
+
+$resultado = mysqli_query($conn, $actualizar);
+if($resultado) {
+    echo "<script>alert('se han cambiado los datos con exito');
+    window.location='/editar.php'</script>";
+}else{
+    echo "<script>alert('no se puedieron actualizar los datos'); window.history.go(-1);</script>";
+}
+?>
