8e2492813e22756ac6a6bb456548a1f9258c58ab
Darkaleja69 <alejandrogona@gmail.com>
2022-10-09 14:32:30 +0200

    Cambios en archivo php en vez de html

--- app/registro.php
+++ app/registro.php
@@ -13,7 +13,18 @@ include("cn.php");
             Registrate en nuestra página web
         </title>
         <link rel="stylesheet" href="estilosRegistro.css" />
+        <link rel="stylesheet" href="listado.css" />
     </head>
+    <header>
+            <div class="texto">
+                <?php
+                    echo '<a href="" class="botonCabecera"> Contacto</a>';
+                    echo '<a href="usuarioIdentificado.php" class="botonCabecera"> Volver</a>';
+                ?>
+                
+            </div>
+            
+        </header>
 
     <body>
         
@@ -23,19 +34,19 @@ include("cn.php");
                     <h1>Registro</h1>
                 <div class="grupo">
                     <input type="text" name="nombre" id="nombre" required><span class="barra"></span>
-                    <label for="">Nombre</label>
+                    <label for="transicion">Nombre</label>
                 </div>
                 <div class="grupo">
                     <input type="text" name="apellidos" id="apellidos" required ><span class="barra"></span>
-                    <label for="">Apellidos</label>
+                    <label for="transicion">Apellidos</label>
                 </div>
                 <div class="grupo">
                     <input type="text" name="dni" placeholder="11111111-A" id="dni" required ><span class="barra"></span>
-                    <label for="">DNI</label>
+                    <label for="transicion">DNI</label>
                 </div>
                 <div class="grupo">
                     <input type="text" name="telefono" id="telefono" @blur="showDatepicker" required ><span class="barra"></span>
-                    <label for="">Telefono</label>
+                    <label for="transicion">Telefono</label>
                 </div>
                 <div class="grupo">
                     <input type="date" name="fechaN" id="fecha_nac" required ><span class="barra"></span>
@@ -43,15 +54,15 @@ include("cn.php");
                 </div>
                 <div class="grupo">
                     <input type="email" name="email" placeholder="nombre@servidor.extension" id="email" required ><span class="barra"></span>
-                    <label for="">E-mail</label>
+                    <label for="transicion">E-mail</label>
                 </div>
                 <div class="grupo">
                     <input type="password" name="clave" id="password1" required ><span class="barra"></span>
-                    <label for="">Contraseña</label>
+                    <label for="transicion">Contraseña</label>
                 </div>
                 <div class="grupo">
                     <input type="password" name="clave2" id="password12" required><span class="barra"></span>
-                    <label for="">Repita la contraseña</label>
+                    <label for="transicion">Repita la contraseña</label>
                 </div>
                 
                 <button type="submit">Regístrate</button>
