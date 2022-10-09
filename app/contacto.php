f0752d45af9affa5e95131cea802ac08a024f9f4
Darkaleja69 <alejandrogona@gmail.com>
2022-10-09 17:38:16 +0200

    Página para contactarnos

---
+++ app/contacto.php
@@ -0,0 +1,37 @@
+<?php
+include("cn.php");
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Contacta con nosotros </title>
+        <link rel="stylesheet" href="contacto.css">
+        <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container-form">
+            <div class="info-form">
+                <h2> Contacta con nosotros </h2>
+                <p>Siempre queremos estar al tanto de los problemas que os surjan al usar nuestra web. Si tenéis cualquier duda sobre nuestros trasteros o los contratos de alquiler que ofertamos, preguntádnos sin pestañear. </p>
+                <a href="#"> <i class="fa fa-phone"></i> 944-22-55-77
+                </a>
+                <a href="#"> <i class="fa fa-envelope"></i> alquilaTuTrastero@gmail.com
+                </a>
+                <a href="#"> <i class="fa fa-map-marked"></i> Barakaldo,Vizcaya,España
+                </a>
+            </div>
+        </div>
+        <form action="#" autocomplete="off">
+            <input type="email" name="email" placeholder="Correo electrónico" class="campo">
+            <input type="text" name="asunto" placeholder="Escribe el asunto del mensaje" class="campo">
+
+            <textarea name="mensaje" placeholder="Escribe tu mensaje" ></textarea>
+
+            <input type="submit" name="enviar" value="Enviar mensaje" class="but-enviar">
+
+
+        </form>
+    </body>
+</html>
