74eeb84f19da88085bb9f08b7c817ad670b03e17
aimarsg <aimar.s.737@gmail.com>
2022-10-11 22:11:24 +0200

    intento de arreglo

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 16-09-2020 a las 16:37:17
--- Versión del servidor: 10.5.5-MariaDB-1:10.5.5+maria~focal
--- Versión de PHP: 7.4.9
+-- Tiempo de generación: 11-10-2022 a las 19:26:56
+-- Versión del servidor: 10.8.2-MariaDB-1:10.8.2+maria~focal
+-- Versión de PHP: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,62 +24,69 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `Trastero`
 --
 
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Estructura de la table usuario
-
-CREATE TABLE 'usuario'('nombre' text not null, 'apellido' text not null, 'DNI' varchar(9) PRIMARY KEY not null, 'telefono' int (9)  not null, 'fechaN' date not null, 'email' varchar(40) not null);
--- EStructura de la tabla trastero 
-
---CREATE TABLE 'trastero'('ID' int(10) PRIMARY KEY not null, 'nombre' varchar(20) not null, 'metrodCuadrados' int not null, 'localizacion' varchar(30) not null, 'dueño' varchar(20) not null);
----- Estructura de tabla para la tabla `trastero`
---
-
-CREATE TABLE `trastero` (
+CREATE TABLE `Trastero` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
-  `metroCadrado` int(11) NOT NULL,
+  `metroCuadrado` int(11) NOT NULL,
   `localizacion` varchar(50) NOT NULL,
   `responsable` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Volcado de datos para la tabla `Trastero`
+--
+
+INSERT INTO `Trastero` (`id`, `nombre`, `metroCuadrado`, `localizacion`, `responsable`) VALUES
+(1, 'pon', 50, 'bilbo', 'mario'),
+(2, 'pin', 36, 'barakaldo', 'juan'),
+(3, 'pun', 46, 'ermua', 'pedro'),
+(4, 'pan', 96, 'madrid', 'txema'),
+(5, 'chui', 37, 'galda', 'mario');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Usuario`
+--
+
+CREATE TABLE `Usuario` (
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `DNI` varchar(10) NOT NULL,
+  `telefono` int(9) NOT NULL,
+  `fechaN` date NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `clave` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `Usuario`
+--
+
+INSERT INTO `Usuario` (`nombre`, `apellido`, `DNI`, `telefono`, `fechaN`, `email`, `clave`) VALUES
+('pepe', 'pepe', '23451223E', 212121122, '2022-10-05', 'pepe@gmail.com', 'pepe'),
+('mario', 'mario', '2656625-F', 56236995, '2003-02-20', 'mario@gmail.com', 'mario'),
+('Alejandro', 'Gómez', '37276341-B', 888888888, '2022-10-08', 'alejandro@gmail.com', 'alejandro'),
+('pepe', 'mono', '56321458E', 56236995, '2002-02-20', 'pepe@gmail.com', '');
+
+--
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `trastero`
+-- Indices de la tabla `Trastero`
 --
-ALTER TABLE `trastero`
+ALTER TABLE `Trastero`
   ADD PRIMARY KEY (`id`);
-COMMIT;
-
--- Volcado de datos para la tabla `usuarios`
---
-
-insert into 'usuario' (`nombre`, `apellido`, `DNI`, `telefono`, `fechaN`, `email`) values( 'mario', 'Almandoz', '87080965E', 876543678, 2002/11/06, 'marioalmandoz@gmail.com');
-
-
-INSERT INTO `usuarios` (`id`, `nombre`) VALUES
-(1, 'mikel'),
-(2, 'aitor');
-
-INSERT INTO `trastero`(`id`, `nombre`, `metroCadrado`, `localizacion`, `responsable`) VALUES (12345689,'primero',25,'bilbo','andres');
---
--- Índices para tablas volcadas
---
 
 --
--- Indices de la tabla `usuarios`
+-- Indices de la tabla `Usuario`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `Usuario`
+  ADD PRIMARY KEY (`DNI`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
