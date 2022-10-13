374ee708af774fc9a870eb94ad5bfec453605cf0
mario <marioalmandoz6@gmail.com>
2022-10-13 12:20:10 +0200

    actualizar

--- app/actualizar.php
+++ app/actualizar.php
@@ -1,5 +1,7 @@
 <?php
+session_start();
 include("cn.php");
+
 
 $nombre = $_POST["nombre"];
 $apellidos = $_POST["apellidos"];
@@ -8,15 +10,18 @@ $telefono = $_POST["telefono"];
 $fechaN = $_POST["fechaN"];
 $email = $_POST["email"];
 $clave = $_POST["clave"];
+$email_Registro = $_SESSION['email'];
+echo $email_Registro;
 
-$actualizar = "UPDATE usuario SET nombre='$nombre', apellido='$apellidos', DNI='$dni', telefono= '$telefono', fechaN='$fechaN', email='$email', contraseña='$clave' where email='' ";
-$resultado = mysqli_fetch_array($conn, $actualizar);
+$actualizar = "UPDATE Usuario SET nombre='$nombre', apellido='$apellidos', DNI='$dni', telefono= '$telefono', fechaN='$fechaN', email='$email', clave='$clave' WHERE email='$email_Registro'";
+$resultado = mysqli_query($conn, $actualizar);
 if($resultado) {
     echo "<script>alert('Se han modificado los datos del usuario con exito');window.location='/usuarioIdentificado.php'</script>";
 
     
 }else{
     echo "<script>alert('No se han podido modificar los datos del usuario'); window.location='/inicio.php'</script>";
+    //
 }
 
 ?>
