d706082bf88d1dd1830942297ced24152a3efa0f
aimarsg <aimar.s.737@gmail.com>
2022-10-30 12:50:03 +0100

    arreglar inyeccion

--- app/identificar.php
+++ app/identificar.php
@@ -7,9 +7,45 @@ $email = $_POST["email"];
 $clave = $_POST["clave"];
 
 //select
-$comprobar = "SELECT * FROM Usuario WHERE email='$email' AND clave='$clave'";
 
-$resultado = mysqli_query($conn, $comprobar);
+if (!($sentencia = $conn->prepare("SELECT * FROM Usuario WHERE email = ? AND clave = ? "))) {
+    echo "Falló la preparación: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+
+
+if (!$sentencia->bind_param("ss", $email, $clave)) {
+    echo "Falló la vinculación de parámetros: (" . $sentencia->errno . ") " . $sentencia->error;
+}
+
+if (!$sentencia->execute()) {
+    echo "Falló la ejecución: (" . $sentencia->errno . ") " . $sentencia->error;
+    echo "<script>alert('No se puede identificar debido a que la contraseña o el usuario son incorrectos.'); window.location='/inicio.php'</script>";
+
+}else {
+    $sentencia-> bind_result($nombre, $apellido, $DNI, $telefono, $fechaN, $email, $clave);
+    $sentencia->fetch();
+    if(strlen($nombre)>0) {
+        //si existe
+        echo "<script>alert('Se ha identificado el usuario con éxito.');window.location='/usuarioIdentificado.php'</script>";
+        //guardar los datos del usuario en la variable de sesion
+        $_SESSION['nombre']=$nombre;
+        $_SESSION['apellido']=$apellido;
+        $_SESSION['DNI']=$DNI;
+        $_SESSION['telefono']=$telefono;
+        $_SESSION['fechaN']=$fechaN;
+        $_SESSION['email']=$email;
+        $_SESSION['clave']=$clave;
+    }else{
+        //si no existe
+        echo "<script>alert('No se puede identificar debido a que la contraseña o el usuario son incorrectos.'); window.location='/inicio.php'</script>";
+        //echo "<script>alert('se ha identificado el usuario con exito');window.location='/usuarioIdentificado.php'</script>";
+    
+    }
+    
+   
+}
+
+/*$resultado = mysqli_query($conn, $comprobar);
 //obtener el nombre del usuario del resultado
 $usuario = mysqli_fetch_array($resultado);
 $nombre=$usuario["nombre"];
@@ -31,6 +67,6 @@ if(strlen($nombre)>0) {
     //echo "<script>alert('se ha identificado el usuario con exito');window.location='/usuarioIdentificado.php'</script>";
 
 }
-
+*/
 
 ?>
