566087bd5858c684f2a7af572fc450a590cdea1a
aimarsg <aimar.s.737@gmail.com>
2022-11-14 19:48:10 +0100

    nada

--- app/inicio.php
+++ app/inicio.php
@@ -4,8 +4,7 @@ ini_set("session.cookie_httponly", True);//httponly flag
 session_start();
 //denegar xframe options
 header('X-Frame-Options: SAMEORIGIN');
-//comprobacion timeout
-include("timeout.php");
+
 
 include("cn.php");
 
