735533e12738de553554f2694d1d71f8ea547235
aimarsg <aimar.s.737@gmail.com>
2022-11-14 20:56:31 +0100

    la cooki samesite de mierd

--- app/inicio.php
+++ app/inicio.php
@@ -1,11 +1,12 @@
 <?php
 ini_set('session.cookie_samesite', 'Strict'); //samesite cookie
-session_set_cookie_params(["SameSite" => "Strict"]); //none, lax, strict
 
 ini_set("session.cookie_httponly", True);//httponly flag
 
 //sesion php y conexion con base de dadtos
 session_start();
+header('Set-Cookie: PHPSESSID= ' . session_id() . '; SameSite=Strict; Secure; HttpOnly');
+
 //denegar xframe options
 header('X-Frame-Options: SAMEORIGIN');
 
